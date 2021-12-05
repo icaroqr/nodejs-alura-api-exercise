9e26cd48bc9e8c8c0c81e276a83da760b4a66c19
Icaro Rezende <icarorez@gmail.com>
2021-12-05 18:33:27 -0300

    feat: Adicionando model de atendimento

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,11 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send("Rota de busca de atendimentos (GET)"))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
-        res.send("Você está na tota de cadastro atendimentos (POST)")
+        const atendimento = req.body
+        Atendimento.adiciona(atendimento)
     })
 }
+
---
+++ infraestrutura/tabelas.js
@@ -0,0 +1,21 @@
+class Tabelas {
+    init(conexao){
+        console.log("Script de tabelas executados")
+       this.conexao = conexao 
+       this.criarAtendimento()
+    }
+
+    criarAtendimento(){
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente VARCHAR(50) NOT NULL, pet VARCHAR(20), servico VARCHAR(20) NOT NULL, status VARCHAR(20) NOT NULL, observacoes TEXT, PRIMARY KEY(id))'
+
+        this.conexao.query(sql, (erro)=>{
+            if(erro){
+                console.log(erro)
+            }else{
+                console.log("Tabela atendimentos criada com sucesso!")
+            }
+        })
+    }
+}
+
+module.exports = new Tabelas
